b1568441521e43b8bf9c72e79327ffd13f01bfef
Dina <zr56@njit.edu>
2019-08-08 18:21:14 -0700

    Added Edit and Delete buttons for Questions.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">Questions
-                        <a class="btn btn-primary float-right" href="#">
+                        <a class="btn btn-primary float-right" href="{{route ('question.create')}}">
                             Create a Question
                         </a>
 
@@ -37,9 +37,7 @@
                                         </div>
                                     </div>
                                 @empty
-                                    <a class="btn btn-primary float-right" href="{{route ('question.create')}}">
                                     There are no questions to view, you can  create a question.
-                                    </a>
                                 @endforelse
                             </div>
                         </div>

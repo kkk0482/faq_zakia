691b0ac74f99d27c016fe65908a8c2e9176cb4e0
Kalpit Khamar <kkk0482@njit.edu>
2019-08-09 23:26:17 -0400

    Notifcation and email error fix

--- app/Notifications/FollowNotified.php
+++ app/Notifications/FollowNotified.php
@@ -1,14 +1,17 @@
 <?php
+
 namespace App\Notifications;
 use App\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
+
 class FollowNotified extends Notification
-//class UserFollowed extends Notification
 {
     use Queueable;
+
+    protected $follower;
     /**
      * Create a new notification instance.
      *
@@ -16,9 +19,9 @@ class FollowNotified extends Notification
      */
     public function __construct(User $follower)
     {
-        //
         $this->follower=$follower;
     }
+
     /**
      * Get the notification's delivery channels.
      *
@@ -27,29 +30,37 @@ class FollowNotified extends Notification
      */
     public function via($notifiable)
     {
-        return ['database'];
+        return ['mail'];
     }
-    public function toDatabase($notifiable)
+
+    public function toMail($notifiable)
     {
-        return [
-            'follower_id' => $this->follower->id,
-            'follower_name' => $this->follower->name,
-        ];
+
+
+        return (new MailMessage)
+            ->greeting('Hello!')
+            ->line( $this->follower->name . ' ' .'is following you');
+
     }
     /**
      * Get the mail representation of the notification.
      *
      * @param  mixed  $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    public function toMail($notifiable)
+    public function toDatabase($notifiable)
     {
-        return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
+    return [
+    'follower_id' => $this->follower->id,
+    'follower_name' => $this->follower->name,
+    ];public function toDatabase($notifiable)
+    {
+    return [
+    'follower_id' => $this->follower->id,
+    'follower_name' => $this->follower->name,
+    ];
     }
-    /**
+    }
+     *  /**
      * Get the array representation of the notification.
      *
      * @param  mixed  $notifiable
